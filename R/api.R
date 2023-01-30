2ca72f964910a9f06bb71f93e7b7166301f0448d
Jonathan Carroll <github@jcarroll.com.au>
2023-01-30 15:11:48 +1030

    enable PATCH requests

--- R/api.R
+++ R/api.R
@@ -65,18 +65,23 @@ get_api_yaml <- function(org = get_org()) {
   yaml::read_yaml(text = yaml)
 }
 
-post_benchling <- function(endpoint, body = NULL, org = get_org(), ...) {
+post_benchling <- function(endpoint, body = NULL, org = get_org(), f = c("POST", "PATCH"),...) {
   stopifnot(!is.null(body))
+  f <- match.arg(f)
+  f <- switch(f,
+              POST = httr::POST,
+              PATCH = httr::PATCH)
   url <- glue::glue("https://{org}.benchling.com/api/v2/")
-  resp <- httr::POST(
+  resp <- f(
     url = glue::glue("{url}/{endpoint}"),
     body = jsonlite::toJSON(body, auto_unbox = TRUE),
     encode = "json",
     httr::authenticate(get_token(), "")
   )
-  if (!(sc <- httr::status_code(resp)) == 201L) {
+  if (!(sc <- httr::status_code(resp)) %in% 200:299) {
     warning("** Request returned status ", sc, call. = FALSE)
     return(invisible(resp))
   }
   jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
 }
+
